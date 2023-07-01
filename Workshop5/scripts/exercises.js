9875c86d99886f383a036ba5ae2c051c3a279f78
Marina <marina.zabica@endava.com>
2023-07-01 09:33:23 +0300

    Initial commit workshop 5

---
+++ Workshop5/scripts/exercises.js
@@ -0,0 +1,125 @@
+// Task 1
+// Check if a string is uppercase.
+
+// Example:
+// Input: upperCaseString = "The quick brown fox jumps over the lazy dog"
+// Expected Result: false
+// Input: "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+// Expected Result: true
+
+// Task 2
+// Remove the spaces from the string, then log the resultant string.
+
+// Example:
+// Input: noSpacesString = "The quick brown fox jumps over the lazy dog"
+// Expected Result: "Thequickbrownfoxjumpsoverthelazydog"
+
+// Task 3
+// Calculate the sum of 2 numbers and log the result.
+
+// Example:
+// Input: a = 2 , b = 3
+// Expected Result: sum = 5
+
+// Task 4
+// Return the square root of a number n .
+
+// Example:
+// Input: n = 9
+// Expected Result: sqrt = 3
+
+// Task 5
+// Log to the console your current age.
+
+// Example:
+// Input: year = 1990
+// Expected Result: "You are ${x} years old"
+
+// Task 6
+// Given 2 variables, return the value of the second variable, multiplied as many times as the value of the first.
+
+// Example:
+// Input: count = 5, multiplyString = "Hello"
+// Expected Result: "HelloHelloHelloHelloHello"
+
+// TASK 7
+// Multiply a given number by eight if it is an even number and by nine otherwise.
+// IF SYNTAX: a > b ? c : d ( translation: if a > b , then c , else d)
+
+// Example:
+// Input: 8
+// Expected Result: 64
+// Input: 5
+// Expected Result: 45
+
+// TASK 8
+// Return the final number, at the given power.
+
+// Example:
+// Input: pow = 4, nr = 2
+// Expected Result: 16
+// Input: pow = 3, nr = 3
+// Expected Result: 27
+
+// TASK 9
+// Reverse a string
+
+// Example:
+// Input: revString = "alabala"
+// Expected Result: "alabala"
+// Input: revString = "ItFactory"
+// Expected Result: "yrotcaFtI"
+
+// Task 10:
+// Log is a string is valid or not based on length.
+// Length should be between 4 and 16 characters (both included).
+
+// Example:
+// Input: "itFactory"
+// Expected Result: true
+// Input: "itf"
+// Expected Result: false
+
+// Task 11
+// Your task is to convert strings so that each word starts with a capital letter.
+
+// Example:
+// Input: "The quick brown fox jumps over the lazy dog"
+// Expected Result: "The Quick Brown Fox Jumps Over The Lazy Dog"
+
+// Task 12
+// Return the first element of a given array.
+
+// Example:
+// Input: [1, 2, 3]
+// Expected Result: 1
+// Input: [80, 5, 100]
+// Expected Result: 80
+
+// Task 13
+// Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
+// Example:
+// Input: 22, 15
+// Expected Result: true
+// Input: 83, 34
+// Expected Result: false
+
+// Task 14
+// Return both the minimum and maximum numbers, of a given Array in that order.
+
+// Example
+// Input: [1, 2, 3, 4, 5]
+// Expected Result: [1,5]
+// Input: [22, 81, 33, 14, 52]
+// Expected Result: [14,81]
+
+// Task 15
+// A group of friends have decided to start a secret society. The name will be the first letter of each of their names, sorted in alphabetical order.
+// Take the array of names and return the name of the secret society.
+
+// Example
+// Input: ["Adam", "Sarah", "Malcolm"]
+// Expected Result: "AMS"
+// Input: ["Phoebe", "Chandler", "Rachel", "Ross", "Monica", "Joey"]
+// Expected Result: CJMPRR
---
+++ Workshop5/scripts/index.js
@@ -0,0 +1,53 @@
+/*
+1. reference external script file in html
+2. add extension Quokka.js Wallaby
+3. practice & watch the course recording
+*/
+
+// 4. pop-up methods: alert, confirm, prompt
+// 5. console.log()
+
+// 6. variables (var, let, const) = >  case sensitive language
+
+/* 7. data types & structures
+numbers ->  1, 5, 10, 100, 2.5
+strings -> 'salut', "ma numesc Marina"
+boolean -> true / false (banks)
+arrays -> []
+objects -> {}
+undefined -> ceva ce nu e definit
+null -> nimic
+*/
+
+/* 8. Math Operators
+Multiply *
+Exponents **
+Divide /
+Modulo/Restul impartirii %  5%2 = 1
+Add +
+Subtract -
+*/
+
+/* 9. Math Methods
+Floor - Rounds down
+Ceil - Rounds up
+Random - numar random intre 0 si 1
+*/
+
+// 10. Increment & Decrement
+
+// 11. arrays and operations on arrays
+
+// 12. conditional operators
+// ==, ===, >, <, <=, >=, !=, !== NaN =not a number
+
+// 13. Date Object
+
+// 15. if statement and for statement
+
+/*
+14. Object =  a group of properties and methods
+        properties = what an object Has
+        methods = what an object can do
+        use. to access properties and methods
+ */
