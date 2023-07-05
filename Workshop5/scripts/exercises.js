c28491fa8c1b86d15c63cd740deb4de1089059e3
Marina <marina.zabica@endava.com>
2023-07-05 23:56:07 +0300

    workshop5 exercises

--- Workshop5/scripts/exercises.js
+++ Workshop5/scripts/exercises.js
@@ -7,6 +7,13 @@
 // Input: "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
 // Expected Result: true
 
+// let keyword = prompt("Introdu stringul");
+// if (keyword != keyword.toUpperCase()) {
+//   console.log("Stringul introdus nu e cu litere mari");
+// } else {
+//   console.log("Stringul introdus contine litere mari");
+// }
+
 // Task 2
 // Remove the spaces from the string, then log the resultant string.
 
@@ -14,12 +21,29 @@
 // Input: noSpacesString = "The quick brown fox jumps over the lazy dog"
 // Expected Result: "Thequickbrownfoxjumpsoverthelazydog"
 
+// let keyword = prompt("Introdu stringul"); // marina silaghi
+// for (let i = 0; i < keyword.length; i++) {
+//   if (keyword[i] == " ") {
+//     keyword = keyword.replace(keyword[i], ""); //cipricojocariu
+//   }
+// }
+// console.log(keyword);
+
 // Task 3
 // Calculate the sum of 2 numbers and log the result.
 
 // Example:
 // Input: a = 2 , b = 3
 // Expected Result: sum = 5
+
+// let firstNumber = prompt("Introdu primul numar");
+// let secondNumber = prompt("Introdu al doilea numar");
+
+// if (!(isNaN(firstNumber) || isNaN(secondNumber))) {
+//   console.log(parseInt(firstNumber) + parseInt(secondNumber));
+// } else {
+//   console.log("Nu ati introdus 2 numere");
+// }
 
 // Task 4
 // Return the square root of a number n .
@@ -35,12 +59,27 @@
 // Input: year = 1990
 // Expected Result: "You are ${x} years old"
 
+// TEMA DE CASA
+
 // Task 6
 // Given 2 variables, return the value of the second variable, multiplied as many times as the value of the first.
 
 // Example:
 // Input: count = 5, multiplyString = "Hello"
 // Expected Result: "HelloHelloHelloHelloHello"
+
+// let myString = prompt("Introdu stringul");
+// let result = myString;
+// let times = parseInt(prompt("Introdu de cate ori sa se repete stringul"));
+// let concatName = "";
+
+// if (!isNaN(times)) {
+//   for (let i = 0; i < times; i++) {
+//     concatName = concatName + result;
+//     myString = concatName; //hellohello
+//   }
+//   console.log(myString);
+// }
 
 // TASK 7
 // Multiply a given number by eight if it is an even number and by nine otherwise.
@@ -71,7 +110,7 @@
 // Expected Result: "yrotcaFtI"
 
 // Task 10:
-// Log is a string is valid or not based on length.
+// Log if a string is valid or not based on length.
 // Length should be between 4 and 16 characters (both included).
 
 // Example:
@@ -86,6 +125,10 @@
 // Example:
 // Input: "The quick brown fox jumps over the lazy dog"
 // Expected Result: "The Quick Brown Fox Jumps Over The Lazy Dog"
+// let myString = "marina silaghi";
+
+// myString.indexOf(" ");
+// daca index 7  diferit de spatiu fa uppercase
 
 // Task 12
 // Return the first element of a given array.
@@ -114,6 +157,18 @@
 // Input: [22, 81, 33, 14, 52]
 // Expected Result: [14,81]
 
+// let myArray = [5, 2, 8, 4, 1];
+// console.log(myArray);
+// for (let i = 0; i < myArray.length; i++) {
+//   myArray[i] = parseInt(myArray[i]);
+// }
+
+// let sortedArray = myArray.sort();
+// console.log(sortedArray);
+
+// let result = [sortedArray[0], sortedArray[sortedArray.length - 1]];
+// console.log(result);
+
 // Task 15
 // A group of friends have decided to start a secret society. The name will be the first letter of each of their names, sorted in alphabetical order.
 // Take the array of names and return the name of the secret society.
@@ -123,3 +178,22 @@
 // Expected Result: "AMS"
 // Input: ["Phoebe", "Chandler", "Rachel", "Ross", "Monica", "Joey"]
 // Expected Result: CJMPRR
+
+// let friend = "denisa";
+// friend[0];
+
+let friends = ["Denisa", "Claudia", "Alexandra", "Cipri", "Alin", "Marina"]; //
+let friendsCapital = [];
+let result = "";
+// AACCDM
+// ACDM
+
+for (let friend of friends) {
+  //friendsCapital = friendsCapital + friend[0].toUpperCase();
+  friendsCapital.push(friend[0].toUpperCase());
+  friendsCapital = friendsCapital.sort();
+}
+for (let letter of friendsCapital) {
+  result = result + letter;
+}
+console.log(result);
--- Workshop5/scripts/index.js
+++ Workshop5/scripts/index.js
@@ -3,11 +3,24 @@
 2. add extension Quokka.js Wallaby
 3. practice & watch the course recording
 */
+//alert("Workshop5");
 
 // 4. pop-up methods: alert, confirm, prompt
+
+// let age = 30;
+// alert("Varsta mea:" + myVar);
+
 // 5. console.log()
 
 // 6. variables (var, let, const) = >  case sensitive language
+// camel-case => firstName; lastName : variabile with let and var;
+// const: UPPER CASE;  SNAKE_UPPERCASE
+//Pascal Case = CarOwner
+
+let name = "marina";
+//console.log(Name);
+const PI_MATH = 3.14;
+const E = 2.3;
 
 /* 7. data types & structures
 numbers ->  1, 5, 10, 100, 2.5
@@ -28,6 +41,40 @@ Add +
 Subtract -
 */
 
+let lastName = "silaghi";
+let firstName = "marina";
+console.log(firstName + " " + lastName);
+
+let concatNames = firstName + " " + lastName;
+concatNames = `${firstName} ${lastName}`;
+
+let noApples = 4;
+concatName = `Eu am ${noApples} mere`;
+console.log(concatName);
+console.log(firstName + noApples);
+
+console.log(firstName[2]);
+
+console.log(firstName.replace(" ", ""));
+
+for (let i = 0; i < firstName.length; i++) {
+  console.log(firstName[i]);
+}
+
+for (let i = firstName.length - 1; i >= 0; i--) {
+  console.log(firstName[i]);
+}
+
+let myNumber = 5;
+myNumber = myNumber - 20;
+myNumber -= 20;
+
+myNumber = myNumber - 1;
+myNumber--;
+
+console.log(firstName.length);
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
 /* 9. Math Methods
 Floor - Rounds down
 Ceil - Rounds up
@@ -38,16 +85,65 @@ Random - numar random intre 0 si 1
 
 // 11. arrays and operations on arrays
 
+let myVar = "marina";
+myVar = parseInt(myVar);
+console.log(myVar);
+
+let age = parseInt(prompt("Ce varsta ai tu?"));
+if (!isNaN(age)) {
+  console.log(age);
+} else {
+  console.log("Introdu un numar valid");
+}
+
+let fruits = ["mar"];
+let fruits2 = ["mar", "para", "banana"];
+
+console.log(fruits[1]);
+console.log(fruits[fruits.length - 1]);
+
+fruits.push("kiwi");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+
+fruits.unshift("mango");
+console.log(fruits);
+
+console.log(fruits2.sort());
+
+console.log(fruits.indexOf("para"));
+console.log(fruits2.sort());
+
 // 12. conditional operators
 // ==, ===, >, <, <=, >=, !=, !== NaN =not a number
+age = 20;
+
+if (age !== 20) {
+  console.log("Esti din generatia z");
+} else if (age > 20) {
+  console.log("Bravo ai peste 20 de ani");
+} else {
+  console.log("Bravo ai sub 20 de ani");
+}
+
+for (let i = 0; i <= fruits2.length - 1; i++) {
+  console.log(fruits2[i]);
+}
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
 
 // 13. Date Object
 
+//let birthday = new Date();
+//console.log(birthday);
+//console.log(birthday.toLocaleString());
+let birthday = new Date(1989, 7, 10, 10, 30, 23, 22);
+console.log(birthday);
 // 15. if statement and for statement
-
-/*
-14. Object =  a group of properties and methods
-        properties = what an object Has
-        methods = what an object can do
-        use. to access properties and methods
- */
