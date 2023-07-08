229bb24ca8392354722cd483d8a01e74775b01d1
Marina <marina.zabica@endava.com>
2023-07-08 09:22:16 +0300

    Workshop 6 initial changes

---
+++ Workshop6/scripts/index.js
@@ -0,0 +1,45 @@
+/*
+Object =  a group of properties and methods
+        properties = what an object Has
+        methods = what an object can do
+        use. to access properties and methods
+ */
+
+
+        
+/*
+Function = a block of code designed to perform a particular task/procedure
+           We invoke a call whenever we want tit to do something.
+*/
+
+
+
+// global and local scope
+// functions have access to anything is global
+
+
+
+// function expression
+// arrow functions
+
+
+
+/*
+this  = reference to a particular object
+the object depends on the immediate context
+ */
+
+
+
+/*
+class= a template for creating objects
+       define what properties and methods they have
+       use a constructor for unique properties
+*/
+
+
+
+/*
+DOM = Document Object Model
+      An interface for changing the content of a page
+*/
