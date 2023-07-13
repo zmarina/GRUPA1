2dc784e11c1d1e0afd48ffc3334cc999718f4d43
Marina <marina.zabica@endava.com>
2023-07-13 15:45:02 +0300

    week 7 theory and exercises

---
+++ Week7/scripts/index.js
@@ -0,0 +1,10 @@
+// add/change HTML elements
+// .innerHTML (vulnerable to XSS attacks)
+// .textContent (more secure)
+
+// Events and add listeners
+// onclick, onload, onmouseover, onmouseup, onmousedown
+
+// .addEventListeners (event,function,useCapture)
+// You can add many event handlers to one element
+// Event the same event that invokes different functions
